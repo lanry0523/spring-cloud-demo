c40b00fb543f7c808e2e8a838e81e86e198d3347
Administrator <sendou00002@163.com>
2020-04-28 10:51:34 +0800

    测试

--- spring-cloud-discovery/src/main/java/org/discovery/DiscoveryAppliactionRun.java
+++ spring-cloud-discovery/src/main/java/org/discovery/DiscoveryAppliactionRun.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryAppliactionRun {
 	public static void main(String[] args) {
-		System.setProperty("spring.devtools.restart.enabled", "false");
+		System.setProperty("spring.devtools.restart.enabled", "false1");
 		SpringApplication.run(DiscoveryAppliactionRun.class, args);
 	}
 }
