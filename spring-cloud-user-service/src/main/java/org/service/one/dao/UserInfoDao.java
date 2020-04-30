d21eb95bf5bfd19510170c9a073ab09491357fb0
lanry0523 <sendou00002@163.com>
2020-04-30 19:06:41 +0800

    测试

--- spring-cloud-user-service/src/main/java/org/service/one/dao/UserInfoDao.java
+++ spring-cloud-user-service/src/main/java/org/service/one/dao/UserInfoDao.java
@@ -1,7 +1,6 @@
 package org.service.one.dao;
 
 import org.service.one.entity.PageData;
-import org.service.one.entity.User_info;
 
 public interface UserInfoDao {
 	PageData selectByPhone(PageData pd);
