b0bf7e0330ce14a1016d6538437ff59d6acfc127
lanry0523 <sendou00002@163.com>
2020-04-30 18:58:17 +0800

    测试

--- spring-cloud-order-service/src/main/java/org/spring/cloud/order/base/BaseController.java
+++ spring-cloud-order-service/src/main/java/org/spring/cloud/order/base/BaseController.java
@@ -3,9 +3,10 @@ package org.spring.cloud.order.base;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.common.Logger;
-import com.common.UuidUtil;
+
 import org.spring.cloud.order.entity.PageData;
+import org.spring.cloud.order.util.Logger;
+import org.spring.cloud.order.util.UuidUtil;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
