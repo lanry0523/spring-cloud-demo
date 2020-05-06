a3563a2a6cff90d0d0f3a3b63bfe557f557c3929
Lan <sendou00002@163.com>
2020-05-06 22:36:06 +0800

    提交

--- spring-cloud-gateway-zuul/src/main/java/org/gateway/GateWayAppliactionRun.java
+++ spring-cloud-gateway-zuul/src/main/java/org/gateway/GateWayAppliactionRun.java
@@ -4,11 +4,12 @@ package org.gateway;
 import filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
-@SpringCloudApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 @EnableZuulProxy
 public class GateWayAppliactionRun {
 	public static void main(String[] args) {
